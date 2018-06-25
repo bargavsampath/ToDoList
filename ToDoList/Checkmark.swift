0dd45fcf748b3e4a6aec112129690f000d88b6fa
BARGAV MUNUSAMY SAMPATH <bargavsampath@tamikas-air-2.fios-router.home>
2018-06-25 00:04:33 -0400

    Adding New Item in ToDoList

---
+++ ToDoList/Checkmark.swift
@@ -0,0 +1,17 @@
+//
+//  Checkmark.swift
+//  ToDoList
+//
+//  Created by BARGAV MUNUSAMY SAMPATH  on 6/24/18.
+//  Copyright © 2018 BARGAV MUNUSAMY SAMPATH . All rights reserved.
+//
+
+import Foundation
+
+class Checkmark {
+    var status = false
+    
+    func toggle(val:Bool) ->Bool {
+        return !val
+    }
+}
